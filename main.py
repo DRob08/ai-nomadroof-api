e693a0cc5a7571bf406ba39f53faec5f86b943aa
Darwin Robinson <Darwin.Robinson8@gmail.com>
2025-04-14 20:26:32 -0400

    remove request main

--- main.py
+++ main.py
@@ -1,12 +1,10 @@
 # main.py
 from fastapi import FastAPI
 from routers import bookings, agent
-from routers import ip  # or whatever you called the file
+
 
 
 app = FastAPI()
-
-app.include_router(ip.router)
 
 app.include_router(bookings.router, prefix="/api")
 app.include_router(agent.router, prefix="/api")
